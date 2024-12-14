5cff594baad5251f976c81c3f5ae44b27e5d1afe
Gabriel Teiga <gabriel.teiga@edu.pucrs.br>
2024-12-14 17:03:36 -0300

    feat: created ping endpoint

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"github.com/gofiber/fiber/v2"
+)
+
+func main(){
+	app := fiber.New()
+	
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"success": true,
+			"message": "pong",
+		})
+	})
+
+	app.Listen(":8080")
+}
